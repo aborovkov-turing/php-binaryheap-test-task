18c41eeb1f15851d3295e30104040bf4e0d34a59
Alexey Borovkov <lucky.aaz@gmail.com>
2019-07-11 16:13:55 +0300

    Add sort flag

--- src/array_median.php
+++ src/array_median.php
@@ -1,13 +1,16 @@
 <?php
 
-
+/**
+ * @param array $input
+ * @return float|null
+ */
 function array_median(array $input): ?float
 {
     if(!$input) return null;
 
     $size = count($input);
 
-    sort($input);
+    sort($input, SORT_NUMERIC);
 
     if ($size % 2) return (float) $input[$size / 2];
     else return (float) ($input[$size / 2] + $input[($size / 2) - 1]) / 2;
